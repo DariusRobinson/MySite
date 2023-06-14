a340fb904a1ba0e583291a82b360f8bae34aa7b1
Darius Robinson <robinson.darius090@gmail.com>
2023-06-13 23:12:08 -0400

    working on the project section, trying to make the cards pop out a little more with my current projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,49 +1,127 @@
+// import React from "react";
+// import Button from "react-bootstrap/Button";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import Card from "react-bootstrap/Card";
+
+// const Projects = () => {
+//   return (
+//     <>
+//       <section id="projectsSection">
+//         <Container
+//           className="d-flex justify-content-center mb-5"
+//           id="ProjTitleContainer"
+//         >
+//           <h1>Projects</h1>
+//         </Container>
+//         <Container className="d-flex justify-content-center p-2">
+//           <Row className="row row-cols-1 row-cols-md-2 g-3">
+//             <Col className="px-2">
+//               <div className="projectCardWrapper">
+//                 <Card className="projectCard" rounded>
+//                   <Card.Img variant="top" src="Image 8-27-22 at 12.21 AM.jpg" />
+//                   <Card.Body>
+//                     <Card.Title>Cinema's Top 251</Card.Title>
+//                     <Card.Text>Description</Card.Text>
+//                     <Button>Check it Out</Button>
+//                   </Card.Body>
+//                 </Card>
+//               </div>
+//             </Col>
+//             <Col className="px-2">
+//               <div className="projectCardWrapper">
+//                 <Card className="projectCard" rounded>
+//                   <Card.Img
+//                     variant="top"
+//                     src="Image 8-27-22 at 12.08 AM.jpeg"
+//                   />
+//                   <Card.Body>
+//                     <Card.Title>Cinema's Top 251</Card.Title>
+//                     <Card.Text>Description</Card.Text>
+//                     <Button>Check it Out</Button>
+//                   </Card.Body>
+//                 </Card>
+//               </div>
+//             </Col>
+//             <Col className="px-2">
+//               <div className="projectCardWrapper">
+//                 <Card className="projectCard" rounded>
+//                   <Card.Img variant="top" src="Image 8-27-22 at 12.27 AM.jpg" />
+//                   <Card.Body>
+//                     <Card.Title>Cinema's Top 251</Card.Title>
+//                     <Card.Text>Description</Card.Text>
+//                     <Button>Check it Out</Button>
+//                   </Card.Body>
+//                 </Card>
+//               </div>
+//             </Col>
+//           </Row>
+//         </Container>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Projects;
+
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Projects = () => {
   return (
     <>
       <section id="projectsSection">
-        <Container id="ProjTitleContainer">
+        <Container
+          className="d-flex justify-content-center mb-5"
+          id="ProjTitleContainer"
+        >
           <h1>Projects</h1>
         </Container>
         <Container className="d-flex justify-content-center p-2">
           <Row className="row row-cols-1 row-cols-md-2 g-3">
             <Col className="px-2">
-              <Card rounded>
-                <Card.Img variant="top" src="Image 8-27-22 at 12.21 AM.jpg" />
-                {/* <Card.Body>
-                <Card.Title>Cinema's Top 251</Card.Title>
-                <Card.Text>Description</Card.Text>
-              </Card.Body> */}
-              </Card>
+              <div className="projectCardWrapper">
+                <Card className="projectCard" rounded>
+                  <Card.Img variant="top" src="Image 8-27-22 at 12.21 AM.jpg" />
+                  <Card.Body>
+                    <Card.Title>Cinema's Top 251</Card.Title>
+                    <Card.Text>Description</Card.Text>
+                    <Button>Check it Out</Button>
+                  </Card.Body>
+                </Card>
+              </div>
             </Col>
             <Col className="px-2">
-              <Card rounded>
-                <Card.Img variant="top" src="Image 8-27-22 at 12.08 AM.jpeg" />
-                {/* <Card.Body>
-                <Card.Title>Cinema's Top 251</Card.Title>
-                <Card.Text>Description</Card.Text>
-              </Card.Body> */}
-              </Card>
+              <div className="projectCardWrapper">
+                <Card className="projectCard" rounded>
+                  <Card.Img
+                    variant="top"
+                    src="Image 8-27-22 at 12.08 AM.jpeg"
+                  />
+                  <Card.Body>
+                    <Card.Title>Cinema's Top 251</Card.Title>
+                    <Card.Text>Description</Card.Text>
+                    <Button>Check it Out</Button>
+                  </Card.Body>
+                </Card>
+              </div>
             </Col>
             <Col className="px-2">
-              <Card rounded>
-                <Card.Img variant="top" src="Image 8-27-22 at 12.27 AM.jpg" />
-                {/* <Card.Body>
-                <Card.Title>Cinema's Top 251</Card.Title>
-                <Card.Text>Description</Card.Text>
-              </Card.Body> */}
-              </Card>
+              <div className="projectCardWrapper">
+                <Card className="projectCard" rounded>
+                  <Card.Img variant="top" src="Image 8-27-22 at 12.27 AM.jpg" />
+                  <Card.Body>
+                    <Card.Title>Cinema's Top 251</Card.Title>
+                    <Card.Text>Description</Card.Text>
+                    <Button>Check it Out</Button>
+                  </Card.Body>
+                </Card>
+              </div>
             </Col>
           </Row>
         </Container>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,46 +7,43 @@ import Col from "react-bootstrap/Col";
 const Skills = () => {
   return (
     <section id="skillsSection">
-      <Container className="d-flex justify-content-center p-2">
+      <Container className="d-flex justify-content-center mb-5 p-2">
         <h1>Skills</h1>
       </Container>
       <Container className="d-flex justify-content-center p-2">
         <Row className="row row-cols-1 row-cols-md-3 g-3">
           <Col>
             <Card id="skillsCard" rounded>
-              <Card.Header className="d-flex justify-content-center">
+              <Card.Header className="skillsCard  d-flex justify-content-center">
                 <h4>Web Developer</h4>
               </Card.Header>
               <Card.Body>
                 <Card.Text>
-                  I'm a joyful Web Developer who loves to laugh, succesful
-                  working both collabrotively in teams and independently.
-                  Leveraging 8+ years of client service and account/operations
-                  management expirience with a personal commitment to thoughtful
-                  technology development and design.
+                  JavaScript | React | Express | Redux | Node.js | HTML | CSS3 |
+                  PostgreSQL | C# | .NET | Ajax | Microsoft SQLServer | Visual
+                  Studio | Visual Studio Code | Azure Portal | Azure Data Studio
+                  | Azure Repos | Git | GitHub | JSON |
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col>
             <Card rounded>
-              <Card.Header className="d-flex justify-content-center">
-                <h4>Account&Operations Management</h4>
+              <Card.Header className="skillsCard d-flex justify-content-center">
+                <h4>Account and Operations Management</h4>
               </Card.Header>
               <Card.Body>
                 <Card.Text>
-                  I'm a joyful Web Developer who loves to laugh, succesful
-                  working both collabrotively in teams and independently.
-                  Leveraging 8+ years of client service and account/operations
-                  management expirience with a personal commitment to thoughtful
-                  technology development and design.
+                  Leadership | Team Management | Decision-making | Communication
+                  | Customer Service | Collaboration | Adaptability | Integrity
+                  | Accountability |
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col>
             <Card id="skillsCard" rounded>
-              <Card.Header className="d-flex justify-content-center">
+              <Card.Header className="skillsCard d-flex justify-content-center">
                 <h4>IT Professional</h4>
               </Card.Header>
               <Card.Body>
