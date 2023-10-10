4dc53ff77fe427ebe73bfdabcef28ab7c85364bd
Darius Robinson <robinson.darius090@gmail.com>
2023-10-09 20:51:36 -0400

    updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -55,7 +55,7 @@ const About = () => {
                   borderColor: "black",
                   color: "black",
                 }}
-                href="https://drive.google.com/file/d/1rOwfDRENEgzk1P6BecAK6Ab1MKRBITH1/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1rOwfDRENEgzk1P6BecAK6Ab1MKRBITH1/view?usp=sharing"
               >
                 Resume
               </Button>
